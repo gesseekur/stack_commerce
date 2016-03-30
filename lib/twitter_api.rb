7149372f101f5c4515695534a9d3352c78616aa5
Jessica Peng <jessicaspeng@gmail.com>
2016-03-30 16:11:18 -0700

    added class variables to both on twitter api.rb

--- lib/twitter_api.rb
+++ lib/twitter_api.rb
@@ -1,13 +1,16 @@
-class TwitterApi
-  def self.our_public_tweets
-    @@client.user_timeline('chrissyteigen', count: 25, exclude_replies: false, include_rts: true)
-  end
+require "twitter"
 
+class TwitterApi
+  
   def self.client
-    @client ||= Twitter::REST::Client.new do |config|
+    @@client ||= Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
       config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
     end
+  end
+  
+  def self.our_public_tweets
+    @@client.user_timeline('chrissyteigen', count: 25, exclude_replies: false, include_rts: true)
   end
 
 

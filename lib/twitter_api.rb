ae2064c4aa6e7c74254b5692eba623b2ea2dc50f
Jessica Peng <jessicaspeng@gmail.com>
2016-03-30 16:14:54 -0700

    initialize variable

--- lib/twitter_api.rb
+++ lib/twitter_api.rb
@@ -1,14 +1,15 @@
 require "twitter"
 
 class TwitterApi
-  
+  @@client = '';
+
   def self.client
     @@client ||= Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
       config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
     end
   end
-  
+
   def self.our_public_tweets
     @@client.user_timeline('chrissyteigen', count: 25, exclude_replies: false, include_rts: true)
   end
